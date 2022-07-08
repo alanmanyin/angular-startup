a9de612adebe9469064694ea152a6dcb1e69148b
Jazz Lau <jazz.kw.lau@prudential.com.hk>
2022-07-08 08:47:35 +0800

    fix: shipping json data path

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -18,7 +18,7 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{ type: string, price: number }[]>('assests/shipping.json')
+    return this.http.get<{ type: string, price: number }[]>('assets/shipping.json')
   }
 
   clearCart() {
